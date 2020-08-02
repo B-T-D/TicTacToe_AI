9e918f75ca5b5f38a3cf0e5600fb2694bc2e6b2c
Ben <bdye71@gmail.com>
2020-08-02 01:15:00 -0400

    Stopping point on GeneralTree. Test coverage is good so far on the methods that have been implemented, many left to implement

--- test/test_general_tree.py
+++ test/test_general_tree.py
@@ -1,19 +1,165 @@
 import unittest
+import random
 
 from tic_tac_toe.general_tree import GeneralTree
 
 class TestNode(unittest.TestCase):
     """Tests for the _Node nested nonpublic class."""
 
+    def setUp(self):
+        self.test_node = GeneralTree._Node("Test element")
+
     def test_init(self):
         """Confirm that a _Node object can be initialized."""
-        test_node = GeneralTree._Node("Test element")
-        self.assertIsInstance(test_node, GeneralTree._Node)
+        self.assertIsInstance(self.test_node, GeneralTree._Node)
 
+    def test_children_datatype(self):
+        """Confirm that the children instance variable is a list."""
+        self.assertIsInstance(self.test_node._children, list)
+        
 class TestPosition(unittest.TestCase):
     """Tests for the Position nested class."""
 
+    def setUp(self):
+        self.test_element = "Test element"
+        self.node = GeneralTree._Node(self.test_element)
+        container = [] # Temporary. Just needs something to pass as container 
+        self.test_position = GeneralTree.Position(container, self.node)
+
     def test_init(self):
+        self.assertIsNotNone(self.test_position)
+        self.assertIsInstance(self.test_position, GeneralTree.Position)
+
+    def test_element(self):
+        """Confirm that the element() method returns the test element's
+        string data."""
+        self.assertEqual(self.test_position.element(), "Test element")
+
+    def test_eq(self):
+        """Confirm that the __eq__ operator overloader method works as
+        intended."""
+        # create new position with object as its node:
+        other_container = []
+        new_position = GeneralTree.Position(other_container, self.node)
+        self.assertEqual(self.test_position, new_position)
+
+class TestTreeConstructors(unittest.TestCase):
+    """Test __init__ and add_root."""
+
+    def setUp(self):
+        self.test_tree = GeneralTree()
+
+    def test_init(self):
+        self.assertIsInstance(self.test_tree, GeneralTree)
+        self.assertIsNone(self.test_tree._root) # root should initialize to None
+        self.assertEqual(self.test_tree._size, 0) # size should be zero
+
+    def test_add_root(self):
+        root = self.test_tree._add_root("Root element")
+        self.assertEqual(self.test_tree._root, root._node)
+        self.assertEqual(self.test_tree._size, 1)
+
+class TestAddChild(unittest.TestCase):
+    """Test _add_child internal method."""
+
+    def setUp(self):
+        self.test_tree = GeneralTree()
+        self.root = self.test_tree._add_root("Root element")
+
+    def test_add_one_child(self):
+        """Confirm that a single child can be added."""
+        child = self.test_tree._add_child(self.root, "First child element")
+        self.assertEqual(self.test_tree._root._children,
+                         [child._node])
+                                    # root's _Node._children should now be a
+                                    # a single-element list containing the
+                                    # node object of the Position child
+
+    def test_add_many_children(self):
+        """Confirm that several children can be added with a shared parent."""
+        expected_children_values = [1, 2, 3, 4, 5]
+        for n in expected_children_values:
+            self.test_tree._add_child(self.root, n)
+        self.assertEqual(len(self.test_tree._root._children), 5)
+        # Confirm the values of the underlying elements in the children list
+        #   match the expected ones:
+        for i in range(len(expected_children_values)):
+            self.assertEqual(self.test_tree._root._children[i]._element,
+                             expected_children_values[i])
+
+class TestDelete(unittest.TestCase):
+    """Tests for the _delete internal method."""
+
+    def setUp(self):
+        self.tree = GeneralTree()
+        self.root = self.tree._add_root("Root element")
+        # add one child
+        self.first_child = self.tree._add_child(
+            self.root, "First child element")
+
+    def test_delete_one(self):
+        """Test the simplest case of deleting a leaf that isn't the root."""
+        self.tree._delete(self.first_child)
+        self.assertEqual(self.tree._size, 1)
+
+    def test_delete_node_with_exactly_one_child_leaf(self):
+        """Test that when a node with exactly one child is deleted, that
+        child is promoted to become the child of node that had been its
+        grandparent."""
+        grandchild = self.tree._add_child(self.first_child,
+                                         "grandchild element")
+        self.tree._delete(self.first_child)
+        self.assertEqual(self.tree._size, 2) # Should be 2 nodes now
+        self.assertEqual(grandchild._node._parent, self.root._node)
+
+    def test_delete_node_with_exactly_one_nonleaf_child(self):
+        """Does the 'promotion' still work correctly if the promoted node has
+        multiple layers of its own children and grandchildren?"""
+        random.seed(3)
+        grandchild = self.tree._add_child(self.first_child,
+                                         "grandchild element")
+        # give grandchild multiple children
+        great_grandchildren = [] # list to store these positions for later
+        for i in range(5):
+            ggc = self.tree._add_child(grandchild, i) # Element is just the int
+            great_grandchildren.append(ggc)
+        size_check = self.tree._size
+        for ggc in great_grandchildren:
+            randval = random.random() # Make some be leaves, others not
+            if randval >= 0.5:
+                for i in range(3):
+                    self.tree._add_child(ggc, i)
+        peak_size = self.tree._size
+        assert self.tree._size == size_check + 9, f"size was {self.tree._size}"
+        # with seed 3, should have added 3 (3 of the vals should've been over 0.5)
+        #   Should have added 9 total, 3 * 3.
+        self.tree._delete(self.first_child)
+        self.assertEqual(self.tree._size, peak_size - 1) # Should only have
+                                                        # decreased by 1
+        self.assertEqual(grandchild._node._parent, self.root._node)
+        for ggc in great_grandchildren: # all greatgrandchildren should have same
+                                        #   parent they started with.
+            self.assertEqual(ggc._node._parent, grandchild._node)
+        
+        
+        
+            
+
+class TestPublicAccessors(unittest.TestCase):
+    """Test that the main public accessor methods work for a simple test
+    tree with 3 layers.
+    """
+
+    def setUp(self):
+        self.tree = GeneralTree()
+        # Create a 4-layer tree with non-uniform numbers of children. 4th layer
+        #   is a single element, all other children of depth 3 are leaves.
+
+        
+        
+        
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
--- tic_tac_toe/general_tree.py
+++ tic_tac_toe/general_tree.py
@@ -10,6 +10,9 @@ class GeneralTree:
         __slots__ = '_element', '_parent', '_children' # to make lighter in memory
         #   Will the _children thing work for an arbitrary number of children?
 
+        # Is there some smarter data structure to use than a list-array for
+        # children? Maybe a hash table since we're assuming nonordered
+        # children?
         def __init__(self, element, parent=None, children=None):
             """
             Args:
@@ -23,7 +26,7 @@ class GeneralTree:
             """
             self._element = element
             self._parent = parent
-            self._children = children
+            self._children = children if children is not None else []
 
     # ----------------------- nested Postiion class --------------------------
 
@@ -32,7 +35,134 @@ class GeneralTree:
 
     class Position:
         """Abstraction representing the location of a single element."""
-        pass
+
+        def __init__(self, container, node):
+            """Constructor not meant to be invoked by external user."""
+            self._container = container
+            self._node = node
+
+        def element(self):
+            """Return the element stored at this Position."""
+            return self._node._element
+
+        def __eq__(self, other):
+            """Return True if other is a Position representing the same
+            location.
+
+            Args:
+                other (object): Any type of object
+
+            Returns:
+                (bool): True if other is a Postition representing the same
+                    location, else False.
+            """
+            return type(other) is type(self) and other._node is self._node
+
+    def _validate(self, p):
+        """Validate position and return the associated node if valid.
+
+        Returns:
+            (_Node): the _Node object at position, if position validly exists
+                in the tree.
+        """
+        if not isinstance(p, self.Position):
+            raise TypeError("'p' arg must be proper Position type")
+        if p._container is not self:
+            raise ValueError("'p' arg does not belong to this container")
+        if p._node._parent is p._node: # convention for deprecated nodes
+            raise ValueError("'p' is no longer valid")
+        return p._node
+
+    def _make_position(self, node):
+        """
+        Return Position instance for given node (or None if no node).
+
+        Args:
+            node (_Node): _Node object
+
+        Returns:
+            (Position): New Position instance with node as the value of its
+                _node instance variable.
+        """
+        return self.Position(self, node) if node is not None else None
+
+    # ----------------------- general tree constructor ----------------------
+    def __init__(self):
+        """Create an initially empty general tree."""
+        self._root = None
+        self._size = 0
+
+    # ------------------------- nonpublic updaters ---------------------------
+
+    def _add_root(self, e):
+        """
+        Place element e at the root of an empty tree and return new Position.
+        Raise ValueError if tree nonempty.
+
+        Args:
+            e (object): Element to be stored at root.
+
+        Returns:
+            (Position): New Position object represnting tree's root.
+        """
+        if self._root is not None:
+            raise ValueError('Root exists')
+        self._size = 1
+        self._root = self._Node(e)
+        return self._make_position(self._root)
+
+    def _add_child(self, p: Position, e):
+        """Create a new child for Position p, storing element e. Raise
+        ValueError if Position p is invalid. 
+        
+        Returns:
+            (Position): Position of the new Node.
+        """
+        # append the new Position obj to p._Node._children list
+        node = self._validate(p)
+        self._size += 1
+        new_node = self._Node(element=e, parent=node) # new child _Node object
+        node._children.append(new_node) # add it to parent node's children list
+        return self._make_position(new_node) # make a position object
+
+    def _replace(self, p, e):
+        raise NotImplementedError
+
+    def _attach(self, p: Position, other_tree) -> None:
+        """Attach tree other_tree as a child-subtree of Position p."""
+
+        # Unlike in LBT, we don't care about whether p was external, right?
+        #   Because no limitation on number of children.
+        raise NotImplementedError
+    
+    def _delete(self, p):
+        """
+        Delete the node at Position p, and replace it with its child, if
+        it has exactly one child.
+
+        Return the element that had been stored at Position p. RaiseValueError
+        if Position p is invalid or has more than 1 child.
+        """
+        child = None
+        node = self._validate(p)
+        if self.num_children(p) > 1: raise ValueError("p has multiple children")
+        if self.num_children(p) == 1:
+            child = node._children[0] # better for time or space to list.pop()?
+        if child is not None:
+            child._parent = node._parent # child's grandparent becomes parent
+        if node is self._root:
+            self._root = child # child becomes root
+        else:
+            parent = node._parent
+            # todo not sure the adaptation from the LBT code is correct here,
+            #   wrt skipping the if node is parent._left stuff
+            # Might be fine though. This child's child would be "pulled up"
+            #   with it by the promotion, without any further action, right?
+        self._size -= 1
+        node._parent = node # convention for deprecated node (make it its own parent)
+        return node._element
+
+    
 
     # ----------------------- public accessor methods ------------------------
 
@@ -81,7 +211,10 @@ class GeneralTree:
         Returns:
             (int): Number of chidren nodes of position.
         """
-        pass
+        # Hasty implementation bc needed to test delete, may not be smartest
+        #   But builtin len(list) should run in O(1) no different from
+        #   storing an instance variable num_children for each node.
+        return len(position._node._children)
 
     def children(self, position):
         """Generate an iteration of Positions representing the children nodes
