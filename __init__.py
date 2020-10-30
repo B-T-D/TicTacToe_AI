0730a73cf1e2ae4e760f02afbd1ead42c37974f4
Ben <bdye71@gmail.com>
2020-10-30 18:18:12 -0400

    Delete sys.path modifier things that weren't doing anything to help either unittest or Pytest

--- __init__.py
+++ __init__.py
@@ -1,20 +1,3 @@
 import os, sys
 
-print(f"os.getcwd() is {os.getcwd()}")
-print(f"os.pardir = {os.pardir}")
-
-if os.name == "nt": # If windows use backslash
-    sys.path.append(os.path.abspath("..") + "\\tic_tac_toe")
-    for subdirectory in os.scandir(".."):
-        print(f"subdirectory.name is {subdirectory.name}")
-        print(f"os.path.abspath(subdirectory.name) = {os.path.abspath(subdirectory.name)}")
-        sys.path.append(os.path.abspath(subdirectory.name + "\\"))
-        print(f"appended to path: {subdirectory.name}")
-else: # TravisCI default environment os.name = str "posix"
-    for subdirectory in os.scandir():
-        sys.path.append(os.path.abspath(subdirectory.name + "/"))
-print(f"parent __init__ updated sys.path to:")
-for entry in sys.path:
-    print(f"\t{entry}")
-
 print("parent __init__ ok")
--- test/__init__.py
+++ test/__init__.py
@@ -1,14 +1,5 @@
 import sys
 import os
 
-if os.name == "nt":
-    sys.path.append(os.path.abspath(".") + "\\tic_tac_toe")
-
-
 print(f"test\\__init__.py ran")
 
-print(f"test\\ __init__ updated sys.path to:")
-for entry in sys.path:
-    print(f"\t{entry}")
-
-sys.path.insert(0, "TEST")
