c4e8abec23eb9e22a7eb710260a67afe04c2272e
B-T-D <55849024+B-T-D@users.noreply.github.com>
2021-04-22 15:13:24 -0400

    Update tictactoe.py
    Comments cleanup

--- tictactoe.py
+++ tictactoe.py
@@ -4,13 +4,7 @@
 
 from tic_tac_toe import game
 
-import sys # for PI experiment
-
 def main():
-    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
-        print('Running in a PyInstaller bundle.')
-    else:
-        print('Running in a normal Python process.')
     game.Game().main()
 
 if __name__ == '__main__':
